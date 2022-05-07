f4ba833a309a74a5e02786e5d630489e733cf2ab
vitoleite <vitudhr@gmail.com>
2022-05-07 19:53:37 -0300

    :recycle: refact: Refatoração de código e adicionando type annotations

--- bcb_extract/bcb_selenium.py
+++ bcb_extract/bcb_selenium.py
@@ -1,5 +1,6 @@
+import glob, os
+from xmlrpc.client import DateTime
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,20 +8,20 @@ from time import sleep
 from datetime import datetime, timedelta
 import dateutil.relativedelta
 from bcb_pandas import formatting
-import glob, os
 
 
 class Extract(object):
     
     def __init__(self):
-        self.url = 'https://www3.bcb.gov.br/sgspub/localizarseries/localizarSeries.do?method=prepararTelaLocalizarSeries'
-        self.data_initial = datetime.today().replace(day=1)
-        self.first_day_prev_month = self.data_initial + dateutil.relativedelta.relativedelta(months=-1)
-        self.last_day_prev_month = self.data_initial - timedelta(days=1)
+        self.url            : str = 'https://www3.bcb.gov.br/sgspub/localizarseries/localizarSeries.do?method=prepararTelaLocalizarSeries'
+        self.data_initial   : DateTime = datetime.today().replace(day=1)
+        self.first_day_prev_month : DateTime = self.data_initial + dateutil.relativedelta.relativedelta(months=-1)
+        self.last_day_prev_month  : DateTime = self.data_initial - timedelta(days=1)
 
-        self.data_inicio = self.first_day_prev_month.strftime('%d%m%Y')
-        self.data_fim = self.last_day_prev_month.strftime('%d%m%Y')
-    
+        self.data_inicio    : str = self.first_day_prev_month.strftime('%d%m%Y')
+        self.data_fim       : str = self.last_day_prev_month.strftime('%d%m%Y')
+
+
     def Browser(self):
         """
         Processo feito para extrair as informações da Poupança.
@@ -29,8 +30,7 @@ class Extract(object):
         - Baixar o arquivo
         """
 
-        driver = webdriver.Chrome()
-        driver.maximize_window()
+        driver = webdriver.Chrome().maximize_window()
 
         driver.get(self.url)
 
@@ -80,11 +80,11 @@ class Extract(object):
         """
         # Data refactoring
 
-        file = max(glob.iglob("C:/Users/vitor/Downloads/*.csv"), key=os.path.getctime)
-        save_local = 'scrapy/selenium/bcb_extract/data/'
+        file        : str = max(glob.iglob("C:/Users/vitor/Downloads/*.csv"), key=os.path.getctime)
+        save_local  : str = 'scrapy/selenium/bcb_extract/data/'
         
-        file_name = 'dados_poupanca_'
-        month_year = self.first_day_prev_month.date().strftime("%b%y")
+        file_name   : str = 'dados_poupanca_'
+        month_year  : str = self.first_day_prev_month.date().strftime("%b%y")
 
         formatting(file=file, file_name=file_name, month_name=month_year, save_local=save_local)
 
